7b30f6789809be49a86005b30454edfb16e7d86e
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-07 19:57:12 -0400

    removing comments

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// server.js
-
-// set up ======================================================================
-// get all the tools we need
 const express  = require('express');
 const app      = express();
 const port     = process.env.PORT || 3000;
@@ -18,40 +14,34 @@ const session      = require('express-session');
 
 const configDB = require('./config/database.js');
 
-// configuration ===============================================================
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
-// connect to our database
 mongoose.connect(configDB.url, (err, database) => {
   if (err) return console.log(err)
   connect(database)
 });
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs');
 
-// required for passport
-require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport);
 app.use(session({
-    secret: 'rcbootcamp2019c', // session secret
+    secret: 'rcbootcamp2019c',
     resave: true,
     saveUninitialized: true
 }));
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.session());
+app.use(flash());
 
 
-// routes ======================================================================
-const connect = (db) => require('./app/routes.js')(app, passport, db, multer, ObjectId); // load our routes and pass in our app and fully configured passport
+const connect = (db) => require('./app/routes.js')(app, passport, db, multer, ObjectId);
 
 
-// launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
